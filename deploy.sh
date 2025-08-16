ae6c319a805f4a18298456a79bff307fa6abf26c
Klaus Quibeldey-Cirkel <klaus.quibeldey-cirkel.ext@iu.org>
2025-08-16 17:39:29 +0200

    chore: remove obsolete Makefile and deploy script

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Datei obsolet. Deployment erfolgt jetzt ausschließlich über:
-#   git subtree push --prefix mc_test_app <remote> main
-# Diese Datei kann gelöscht werden:
-#   git rm mc_test_app/deploy.sh && git commit -m "remove obsolete deploy script"
